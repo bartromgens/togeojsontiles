34673a2170f6b5240bacb9a529d40d27540575a4
Bart Römgens <bart.romgens@gmail.com>
2016-04-29 17:41:21 +0200

    Create initial togeojsontiles python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='togeojsontiles',
+    version='0.1',
+    description='Convert gpx and geojson to geojson tiles using tippecanoe (C++) and togeojson (javascript)',
+    url='http://github.com/bartromgens/togeojsontiles',
+    author='Bart Römgens',
+    author_email='bart.romgens@gmail.com',
+    license='MIT',
+    packages=['togeojsontiles'],
+    zip_safe=False
+)
